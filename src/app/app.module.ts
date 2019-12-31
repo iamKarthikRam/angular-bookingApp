48dc3dc0dfb9280e6c4028bd4e7d28426fb15157
iamKarthikRam <karthikbeece@gmail.com>
2019-12-31 12:55:56 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppRouteModule } from './app.route.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { InitialLoadPageComponent } from './initial-load-page/initial-load-page.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRouteModule],
-  declarations: [ AppComponent, LoginComponent,InitialLoadPageComponent],
+  declarations: [ AppComponent, LoginComponent,InitialLoadPageComponent, HeaderComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
