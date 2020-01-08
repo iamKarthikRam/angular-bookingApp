a3aa855eae28d74a1d9e89a0142511ded49ae721
iamKarthikRam <karthikbeece@gmail.com>
2020-01-08 12:26:01 +0530

    4 files modified, 2 added - SignUp Form Creation & Styles

--- src/app/initial-load-page/initial-load-page.component.ts
+++ src/app/initial-load-page/initial-load-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ModalDirective } from 'ng2-bootstrap';
 
 @Component({
   selector : 'initial-load-page',
   templateUrl : './initial-load-page.component.html'
 })
-export class InitialLoadPageComponent {
+export class InitialLoadPageComponent implements OnInit{
+
+  isLoginForm : boolean;
+
+  ngOnInit(){
+    this.isLoginForm = true;
+  }
+
+  loadSignupForm(){
+    this.isLoginForm = false;
+  }
+
+  onSignupSubmit(signupForm:any){
+    console.log(signupForm);
+  }
 
 }
